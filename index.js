7cb20971ea0bfa31da2e6e6c3f81eb9daf91f495
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-07-14 14:58:26 -0600

    commit tree e209da6c464f74811f7fdee97b6987a76a774506

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let divide = () => 2000 / 100;
+// let divide = () => 2000 / 100;
 
-let square = x => x * x;
+// let square = x => x * x;
 
-let add = (a, b) => a + b;
+// let add = (a, b) => a + b;
+
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = x => x * x;
+
+const add = (x , y) => x + y;
